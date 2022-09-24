f6bb19c4d9361adf7d03f55f23d150840f4d482d
Lily Russ <llyruss@yahoo.com>
2022-09-24 12:56:46 -0500

    added countdown timer. Displayed correct or incorrect and each question is answered

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,20 +7,36 @@ let question3 = document.getElementById("question3");
 let question4 = document.getElementById("question4");
 let question5 = document.getElementById("question5");
 let questions = [question1, question2, question3, question4, question5];
+let messageDiv = document.getElementById("correct")
+let timer = document.getElementById("timer")
+let timeRemaining = 76
 let currentQuestion = 0;
 let options = document.querySelectorAll("h4");
+let lastQuestion = false
 console.log(options)
 
 //on start quiz hide Code Quiz Challenge and instuctions and start quiz button
-//on start quiz display question
-//on start quiz countdown clock starts
 
 startButton.addEventListener("click", function () {
+    startTimer()
     codeQuiz.classList.add("hide")
     instructions.classList.add("hide")
     startButton.classList.add("hide")
     loadQuestion()
 });
+
+//countdown clock decrements -1 every second, stop if last question is answered
+function startTimer(){
+let x = setInterval(function(){
+    timeRemaining--
+    timer.textContent=timeRemaining
+    if( timeRemaining === 0 || lastQuestion === true ){
+        clearInterval(x)
+    }
+},1000)
+}
+
+//on start quiz display question
 function loadQuestion() {
     questions[currentQuestion].classList.remove("hide")
     if (currentQuestion > 0) {
@@ -32,15 +48,19 @@ function loadQuestion() {
 
 for (let i = 0; i < options.length; i++) {
     options[i].addEventListener("click", function(e){
-        console.log(e.target.getAttribute("value"))
+        if (e.target.getAttribute("value") === "correct"){
+        messageDiv.textContent= "Correct!"
+        } else{
+        messageDiv.textContent= "Incorrect"
+        }
+        
         if (currentQuestion<questions.length){
             loadQuestion()
-        }else {console.log("quiz over!")}
-
+        }else{ lastQuestion = true
+        console.log("quiz over!")
+        }
     })
 }
-
-//countdown clock decrements -1 every second
 
 //on click of answer, show correct or incorrect for 1 second, then display next question and hide current qustion
 //if incorrect, deduct 15 seconds from countdown
